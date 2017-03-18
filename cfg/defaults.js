3213b5d1a0217b98777de608a7922ccdd6963e4f
ckinmind <245160421@qq.com>
2017-03-19 00:00:57 +0800

    edit config

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -28,18 +28,18 @@ function getDefaultModules() {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
       },
-      {
-        test: /\.sass/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
-      },
-      {
-        test: /\.scss/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
-      },
-      {
-        test: /\.less/,
-        loader: 'style-loader!css-loader!less-loader'
-      },
+      // {
+      //   test: /\.sass/,
+      //   loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+      // },
+      // {
+      //   test: /\.scss/,
+      //   loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+      // },
+      // {
+      //   test: /\.less/,
+      //   loader: 'style-loader!css-loader!less-loader'
+      // },
       {
         test: /\.styl/,
         loader: 'style-loader!css-loader!stylus-loader'
