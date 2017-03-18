c9f2f764fe8de03070c5a5f236aca8567f047e15
ckinmind <245160421@qq.com>
2017-03-18 23:56:51 +0800

    add layout

--- src/components/main.js
+++ src/components/main.js
@@ -1,22 +1,61 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
-
 import React from 'react';
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
+import '../styles/app.styl';
 
-let yeomanImage = require('../images/yeoman.png');
 
-class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
+class App extends React.Component {
+
+    state = {
+        collapsed: false,
+    };
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+
+    render() {
+        return (
+            <Layout>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}
+                >
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span className="nav-text">nav 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="video-camera" />
+                            <span className="nav-text">nav 2</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="upload" />
+                            <span className="nav-text">nav 3</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Icon
+                            className="trigger"
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.toggle}
+                        />
+                    </Header>
+                    <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        );
+    }
 }
 
-AppComponent.defaultProps = {
-};
 
-export default AppComponent;
+
+export default App;
